79fdf7d7e8b8811008fce72cd5977e4650a7df8e
Manh Hung <etudeofmemories8698@gmail.com>
2019-05-21 11:24:26 +0700

    start of week 2

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g, \
-    jsonify, current_app
+from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
 from sqlalchemy.orm.exc import NoResultFound
 
 from flask_login import current_user, login_required, login_user, logout_user
